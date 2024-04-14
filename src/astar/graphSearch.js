82157ec7e6f6badd76cbb0d41ed585de55c01644
Malik Kotb <malikkotb@icloud.com>
2024-04-14 10:58:37 +0200

    pq class

--- src/astar/graphSearch.js
+++ src/astar/graphSearch.js
@@ -1,16 +1,23 @@
 export class aStar {
-    
   // Edge Weights: Represent the Euclidean distance between connected nodes.
   // g-Cost: Cumulative distance traveled from the start node along the explored path.
   // h-Cost: Estimated distance from the current node to the goal node, based on Euclidean distance.
   // f-Cost: Total estimated cost of reaching the goal node via the current node, sum of g-cost and h-cost.
 
-  constructor(adjacencyList) {
-    this.adjacencyList = adjacencyList;
-  }
+  findPaths(adjacencyList, start, target) {
+    const openSet = [];
+    // TODO: change openSet from arrray to priority queue with min-heap implemenation
+    // https://medium.com/@adityakashyap_36551/priority-queue-in-javascript-binary-heap-076d0d38703f#:~:text=In%20JavaScript%2C%20there%20is%20no,to%20use%20a%20binary%20heap.
+    const closedSet = [];
+    let startNode = { node: start.midpoint, connectioins: adjacencyList.get(start.midpoint) };
+    openSet.push(startNode);
 
-  findPaths() {
-    console.log(this.adjacencyList);
+    // get connected nodes for a node in openSet: adjacencyList.get(node)
+    console.log(startNode);
+
+    // while (this.openSet.length > 0) {
+    //   // let current = // current node in openSet wit lowest f_cost
+    // }
   }
 
   // Function to calculate Euclidean distance between two nodes
--- src/astar/main.js
+++ src/astar/main.js
@@ -236,7 +236,6 @@ window.addEventListener("load", () => {
         }
       });
     });
-    console.log("pointsToReflect", pointsToReflect);
 
     // reflect centroids on convexEdges
     let reflectedPoints = [];
@@ -298,24 +297,23 @@ window.addEventListener("load", () => {
         // to add an edge (undirected), I need to update the entries for the baseNode and the connectedNode
         adjacencyList
           .get(keyBaseNode)
-          .push({ node: keyConnectedNode, weight: calculateDistance(keyBaseNode, keyConnectedNode) });
+          .push({ node: keyConnectedNode, weight: calculateDistance(keyBaseNode, keyConnectedNode), gCost: 0, hCost: 0 });
         adjacencyList
           .get(keyConnectedNode)
-          .push({ node: keyBaseNode, weight: calculateDistance(keyConnectedNode, keyBaseNode) }); // do inverse of line above to update the connectedNode also
+          .push({ node: keyBaseNode, weight: calculateDistance(keyConnectedNode, keyBaseNode), gCost: 0, hCost: 0 }); // do inverse of line above to update the connectedNode also
       }
     });
 
     // printMap(adjacencyList);
 
-    // 7. Perform pathfinding (graph search algorithm) on adjacency list
-    astar = new aStar(adjacencyList);
-    paths = astar.findPaths();
-
-    // POST-PROCESSING
-    // edgeConnections.map((edge))
     let startNode = edgeConnections[0].startNode;
     let targetNode = edgeConnections[0].targetNode;
 
+    // 7. Perform pathfinding (graph search algorithm) on adjacency list
+    astar = new aStar();
+    paths = astar.findPaths(adjacencyList, startNode, targetNode); // pass in the midpoint, as those represent nodes in the adjacency list (graph)
+
+    // 8. POST-PROCESSING (Rendering the edges)
     edgeConnections.map((edge) => {
       // ACTUALLY I DONT NEED TO CARE ABOUT THE CELLS AT ALL ANYMORE
       // THEY'RE JUST THERE FOR THE PREVIOUS APPROACH
@@ -449,7 +447,7 @@ window.addEventListener("load", () => {
       }
       const midpoint = { x: x + width / 2, y: y + height / 2 };
       nodeMidpoints.push(midpoint);
-      return { x, y, width, height };
+      return { x, y, width, height, midpoint: midpoint };
     });
   }
 
