005e09018e4a1ab820e914ddc63afbec75ba24d8
Malik Kotb <malikkotb@icloud.com>
2024-04-16 17:06:20 +0200

    exisitng opposite edge, opposite target and start nodes

--- src/astar/graphSearch.js
+++ src/astar/graphSearch.js
@@ -9,6 +9,7 @@ export class aStar {
     this.adjacencyList = adjacencyList;
     this.nodeMidoints = nodeMidoints;
     this.centroidsOnPaths = [];
+    this.paths = [];
   }
 
   findPath(start, target) {
@@ -30,6 +31,20 @@ export class aStar {
     gScore[JSON.stringify(start)] = 0;
     fScore[JSON.stringify(start)] = this.calculateDistance(start, target);
 
+    // TODO: check if they have same start and end node (just flipped) so opposite
+    // -> adjust tentativeGScore
+    let oppositePathExists = false;
+    this.paths.forEach((path) => {
+      if (this.areEndPoints(start, target, path)) {
+        oppositePathExists = true;
+      }
+    });
+    if (oppositePathExists) {
+      console.log("exisiting paths: ", JSON.stringify(this.paths));
+      console.log(start, target);
+      console.log("opposite edge");
+    }
+
     while (!openSet.isEmpty()) {
       let current = openSet.dequeue(); // get current node in openSet wit lowest f_cost
 
@@ -43,28 +58,21 @@ export class aStar {
         let tentativeGScore = gScore[JSON.stringify(current)] + this.calculateDistance(neighbour, current);
 
         // check if neighbour is a centroid that's already part of another path
-        // to avoid crossong edges
+        // to avoid crossing edges
         // this is a desirable condition but not necessary as there will inevitably be some edge crossing
         // especially with larger graphs and many connections
+
         const neighbourIsCentroidOfExisitngPath = this.isNodeInArray(neighbour, this.centroidsOnPaths);
         if (neighbourIsCentroidOfExisitngPath) {
+          // if (oppositePathExists) {
+          //   console.log(start, target);
+          //   console.log("opposite edge");
+          // }
           tentativeGScore += 100;
         }
 
-
         // check if a neighbour (THAT IS NOT THE TARGETNODE AND NOT THE STARTNODE) is a nodeMidpoint -> then set edge weight to that neighbour high; as we dont want to go through another node
-        let nodeMidpointNotTargetNotStart = null;
-        const neighbourIsMidpointAndNotTarget = this.nodeMidoints.some((nodeMidoint) => {
-          nodeMidpointNotTargetNotStart = nodeMidoint;
-          return (
-            neighbour.x !== start.x &&
-            neighbour.y !== start.y &&
-            neighbour.x !== target.x &&
-            neighbour.y !== target.y &&
-            neighbour.x === nodeMidoint.x &&
-            neighbour.y === nodeMidoint.y
-          );
-        });
+        const neighbourIsMidpointAndNotTarget = this.isNeighbourMidpoint(neighbour, start, target, this.nodeMidoints);
         if (neighbourIsMidpointAndNotTarget) {
           tentativeGScore += 1000;
         }
@@ -91,12 +99,40 @@ export class aStar {
       totalPath.unshift(current);
     }
     this.centroidsOnPaths.push(...totalPath.slice(1, -1));
+    this.paths.push(totalPath);
     return totalPath;
   }
 
   // Function to check if a specific node is in the array
   isNodeInArray(node, array) {
     return array.some((n) => n.x === node.x && n.y === node.y);
+  }
+
+  isNeighbourMidpoint(neighbour, start, target, nodeMidpoints) {
+    return nodeMidpoints.some((nodeMidpoint) => {
+      return (
+        neighbour.x !== start.x &&
+        neighbour.y !== start.y &&
+        neighbour.x !== target.x &&
+        neighbour.y !== target.y &&
+        neighbour.x === nodeMidpoint.x &&
+        neighbour.y === nodeMidpoint.y
+      );
+    });
+  }
+
+  areEndPoints(point1, point2, pathArray) {
+    const firstPoint = pathArray[0];
+    const lastPoint = pathArray[pathArray.length - 1];
+    // Check if the given points match either the first and last points in the array
+    // or the last and first points in reverse order
+    return (
+      (point1.x === firstPoint.x &&
+        point1.y === firstPoint.y &&
+        point2.x === lastPoint.x &&
+        point2.y === lastPoint.y) ||
+      (point1.x === lastPoint.x && point1.y === lastPoint.y && point2.x === firstPoint.x && point2.y === firstPoint.y)
+    );
   }
 
   // Function to calculate Euclidean distance between two nodes
